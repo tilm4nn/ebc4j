268d30a8b9826ccd783a6e9dd12be3f0a9b3757a
tilm4nn <tilmann@object-zoo.net>
2012-02-13 16:42:26 +0100

    allowed flow execution with concrete flow instances

--- src/net/objectzoo/ebc/executor/FlowExecutor.java
+++ src/net/objectzoo/ebc/executor/FlowExecutor.java
@@ -20,7 +20,8 @@ public class FlowExecutor
 																					  InputParameter input)
 	{
 		FlowType flow = ebcFlowCreator.createFlow(flowClass);
-		flow.processAction().invoke(input);
+		
+		process(flow, input);
 	}
 	
 	public <InputParameter, OutputParameter, FlowType extends ProcessAndResultFlow<InputParameter, OutputParameter>> OutputParameter processAndReturnResult(Class<FlowType> flowClass,
@@ -28,6 +29,32 @@ public class FlowExecutor
 	{
 		FlowType flow = ebcFlowCreator.createFlow(flowClass);
 		
+		return processAndReturnResult(flow, input);
+	}
+	
+	public <FlowType extends CanStart> void start(Class<FlowType> flowClass)
+	{
+		FlowType flow = ebcFlowCreator.createFlow(flowClass);
+		
+		start(flow);
+	}
+	
+	public <OutputParameter, FlowType extends StartAndResultFlow<OutputParameter>> OutputParameter startAndReturnResult(Class<FlowType> flowClass)
+	{
+		FlowType flow = ebcFlowCreator.createFlow(flowClass);
+		
+		return startAndReturnResult(flow);
+	}
+	
+	public static <InputParameter, FlowType extends CanProcess<InputParameter>> void process(FlowType flow,
+																							 InputParameter input)
+	{
+		flow.processAction().invoke(input);
+	}
+	
+	public static <InputParameter, OutputParameter, FlowType extends ProcessAndResultFlow<InputParameter, OutputParameter>> OutputParameter processAndReturnResult(FlowType flow,
+																																								   InputParameter input)
+	{
 		MockAction<OutputParameter> resultContainer = appendResultContainer(flow);
 		
 		flow.processAction().invoke(input);
@@ -35,16 +62,13 @@ public class FlowExecutor
 		return resultContainer.getLastResult();
 	}
 	
-	public <FlowType extends CanStart> void start(Class<FlowType> flowClass)
+	public static <FlowType extends CanStart> void start(FlowType flow)
 	{
-		FlowType flow = ebcFlowCreator.createFlow(flowClass);
 		flow.startAction().invoke();
 	}
 	
-	public <OutputParameter, FlowType extends StartAndResultFlow<OutputParameter>> OutputParameter startAndReturnResult(Class<FlowType> flowClass)
+	public static <OutputParameter, FlowType extends StartAndResultFlow<OutputParameter>> OutputParameter startAndReturnResult(FlowType flow)
 	{
-		FlowType flow = ebcFlowCreator.createFlow(flowClass);
-		
 		MockAction<OutputParameter> resultContainer = appendResultContainer(flow);
 		
 		flow.startAction().invoke();
@@ -52,12 +76,11 @@ public class FlowExecutor
 		return resultContainer.getLastResult();
 	}
 	
-	private <OutputParameter> MockAction<OutputParameter> appendResultContainer(SendsResult<OutputParameter> flow)
+	private static <OutputParameter> MockAction<OutputParameter> appendResultContainer(SendsResult<OutputParameter> flow)
 	{
 		SendsResult<OutputParameter> resultSender = (SendsResult<OutputParameter>) flow;
 		MockAction<OutputParameter> resultContainer = new MockAction<OutputParameter>();
 		resultSender.resultEvent().subscribe(resultContainer);
 		return resultContainer;
 	}
-	
 }
