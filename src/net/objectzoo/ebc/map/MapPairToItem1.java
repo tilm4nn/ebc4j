12359f8e2acfb9a91acc407ee0672c6470a02c3c
Tilmann Kuhn <tilmann@object-zoo.net>
2012-06-17 00:54:02 +0200

    added pair maps

---
+++ src/net/objectzoo/ebc/map/MapPairToItem1.java
@@ -0,0 +1,13 @@
+package net.objectzoo.ebc.map;
+
+import net.objectzoo.ebc.impl.ProcessAndResultBase;
+import net.objectzoo.ebc.util.Pair;
+
+public class MapPairToItem1<T> extends ProcessAndResultBase<Pair<T, ?>, T>
+{
+	@Override
+	protected void process(Pair<T, ?> pair)
+	{
+		sendResult(pair.getItem1());
+	}
+}
